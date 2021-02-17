4c2127044f6ee58d5b376c828e6f247e815c3d9c
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-02-18 01:38:59 +0200

    Added a build map function

--- main.py
+++ main.py
@@ -36,4 +36,26 @@ def sort_by_distance(unsorted_movies_data: dict) -> dict:
     return sorted_movies_data
 
 
+def build_map(movie_sample: dict) -> None:
+    """
+    Build a folium map and save it as an HTML file
+    """
+    folium_map = folium.Map()
+    for idx, map_movie in enumerate(movie_sample.items()):
+        if idx < 10:
+            folium.Marker(location=[*map_movie[1]], popup=map_movie[0]).add_to(folium_map)
+    tile_layers = 'Open Street Map', 'Stamen Terrain', 'Stamen Toner', \
+                  'Stamen Watercolor', 'CartoDB Positron', 'CartoDB Dark_Matter'
+    for tile_layer in tile_layers:
+        folium.raster_layers.TileLayer(tile_layer).add_to(folium_map)
+    folium_map.add_child(folium.LayerControl())
+    folium_map.save(f"{user_year}.html")
 
+
+if __name__ == '__main__':
+    user_year = input("Please enter a year you would like to have a map for: \n")
+    user_location = tuple((int(coordinate) for coordinate
+                           in input("Please enter your location (format: lat, long): \n").split(',')))
+    sorted_movie_sample = sort_by_distance(get_movie_data_from_file('locations.list', user_year))
+    build_map(sorted_movie_sample)
+    print(sorted_movie_sample)
