ca1738a5e35e23744ead5b98269600c77a8fd4fd
Misha <misha20112000web@gmail.com>
2021-02-18 01:35:03 +0200

    Added main.py module and function which reads the file

---
+++ main.py
@@ -0,0 +1,30 @@
+import re
+import folium
+from geopy.geocoders import Nominatim
+from haversine import haversine
+
+
+def get_movie_data_from_file(file_path: str, year: str) -> dict:
+    """
+    Get movie data from file in the following form:
+    {movie_name: (latitude, longitude), ...}
+    """
+    movies_data = {}
+    movie_name_pattern = re.compile(r'".+"')
+    movie_year_pattern = re.compile(r'\(\d{4}\)')
+    place_pattern = re.compile(r'\B\t(\w+)\n')
+    nom = Nominatim(user_agent="map-generation")
+    with open(file_path) as file:
+        for line in file:
+            movie_name = movie_name_pattern.match(line)
+            movie_year = movie_year_pattern.search(line)
+            place = place_pattern.search(line)
+            if movie_year and year in movie_year.group() and movie_name and \
+                    movie_name.group() not in movies_data and place:
+                location = nom.geocode(place.group(1))
+                if location is not None:
+                    movies_data[movie_name.group()] = location.latitude, location.longitude
+    return movies_data
+
+
+
